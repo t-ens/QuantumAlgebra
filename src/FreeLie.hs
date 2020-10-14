44bcf80a137c843a667caa44435552e70c78cc77
Travis Ens <ens.travis@gmail.com>
2020-10-14 16:54:39 -0400

    Implementing Free lie algebra and tests

--- src/FreeLie.hs
+++ src/FreeLie.hs
@@ -1,10 +1,7 @@
 {-# LANGUAGE MultiParamTypeClasses #-} --Needed for field
-{-# LANGUAGE AllowAmbiguousTypes #-}   --Needed for field
 {-# Language FlexibleInstances #-}     --Needed to make Rational instances
 
-module FreeLie
-    ( someFunc
-    ) where
+module FreeLie where
 
 import Data.List
 
@@ -18,12 +15,16 @@ class Field a where
   fAId :: a
   fMId :: a
 
-class Field a => VectorSpace a b where
-  vAdd :: b -> b -> b
+class AbelianGroup a where
+  agAdd :: a -> a -> a
+  agInv :: a -> a
+  agId :: a
+
+class (Field a, AbelianGroup b) => VectorSpace a b where
   vScal :: a -> b -> b
 
-class VectorSpace a b => LieAlgebra a b where
-  lBracket :: b -> b -> b
+--class VectorSpace a b => LieAlgebra a b where
+--  lBracket :: b -> b -> b
 
 
 -- Implementation of Free Lie algebras over the rationals
@@ -38,31 +39,49 @@ instance Field Rational where
   fMId = 1    
 
 --The free vector space with basis b over the field is a list of scalar
---multiples of basis elements(=
-type FreeVec a b = [(a,b)]   
+--multiples of basis elements
+data FreeVec a b = LC [(a,b)] deriving (Eq, Show)
+
+--sort by basis elements for testing equality
+basisSort :: Ord b => (a,b) -> (a,b) -> Ordering
+basisSort (_,b) (_,c) = compare b c
+
 
 --collect terms
-collect :: (Field a, Eq b) => [(a,b)] -> [(a,b)]
-collect [] = []
-collect ((x,y):xs) = (sumFirst (x,y) match) : (collect non)
+collectList :: (Field a, Eq b) => [(a,b)] -> [(a,b)]
+collectList [] = []
+collectList ((x,y):xs) = (sumFirst (x,y) match) : (collectList non)
   where 
     (match, non) = partition (((==) y).snd) xs
     sumFirst a ys = foldr (\s t -> ( (fst s) `fAdd` (fst t), snd s)) a ys
+collect :: (Field a, Eq b) => FreeVec a b -> FreeVec a b
+collect (LC x) = LC $ collectList x
 
-instance (Field a, Eq b) => VectorSpace a (FreeVec a b) where
-  vAdd v1 v2 = collect.concat $ [v1, v2] 
-  vScal f v = map (\(a,b) -> (f `fMult` a,b)) v 
+removeZerosList :: (Field a, Eq a) => [(a,b)] -> [(a,b)]
+removeZerosList = filter (((/=) fAId).fst) 
+removeZeros (LC x) = LC $ removeZerosList x
+
+simplifyList :: (Field a, Eq a, Ord b) => [(a,b)] -> [(a,b)]
+simplifyList = (sortBy basisSort).removeZerosList.collectList
+simplify (LC x) = LC $ simplifyList x
 
 
+instance (Field a, Eq a, Ord b) => AbelianGroup (FreeVec a b) where
+  agAdd (LC v1) (LC v2) = LC (simplifyList.concat $ [v1, v2])
+  agInv (LC z)  =LC $ map (\(x,y)->(fNeg x,y)) z
+  agId = LC []
+
+instance (Field a, Eq a, Ord b) => VectorSpace a (FreeVec a b) where
+  vScal f (LC v) = LC $  map (\(c,d) -> (f `fMult` c,d)) v 
 
 -- Lyndon Words are a basis of the rational Free Lie algebra
 -- A Lyndon word is lexicographically smaller than all of its proper right
 -- factors
-newtype LW = LW {theWord :: [Int]} deriving (Eq,Ord,Show)
+newtype LW = LW {theWord :: [Int]} deriving (Eq,Ord)
 
 lyndonQ :: LW -> Bool
 lyndonQ  (LW []) = False
-lyndonQ (LW w) = foldr (\x -> (&&) ((LW w) < (LW x))) True [drop i w | i <- [1 .. (length w-1)]]
+lyndonQ LW w = and 
 
 
 allLyndonWords n alph = filter lyndonQ ( map LW (allWords n alph) )
@@ -80,20 +99,39 @@ lyndonFactorization (LW w)
       ( LW $ (reverse.drop (length rf).reverse) w, LW rf )
   | otherwise = (LW [], LW [])
 
-bracket :: (FreeVec Rational LW) -> (FreeVec Rational LW) -> (FreeVec Rational LW)
-bracket x y = x
+bracketForm :: LW -> String
+bracketForm (LW []) = ""
+bracketForm (LW [c]) = show c
+bracketForm x = "[" ++ (bracketForm a) ++ "," ++ (bracketForm b) ++ "]"
+  where (a,b) = lyndonFactorization x
 
-lwAdjoint :: LW -> LW -> FreeVec Rational LW 
-lwAdjoint w = (\z ->
-  if w==z
-    then [(0,w)]
-    else if z < w
-      then (fNeg fMId) `vScal` (bracket [(1,z)] [(1,w)])
+instance Show LW where
+  show = bracketForm
+  
+
+bracket :: (FreeVec Rational LW) -> (FreeVec Rational LW) -> (FreeVec Rational LW)
+bracket (LC xs) (LC ys) = simplify.concatLC $ 
+  map (\ ((a,v),(b,w)) -> ((a `fMult` b) `vScal` (lwAdjoint v w))) 
+    [ (x,y) | x<-xs, y<-ys ]
+      where concatLC :: [FreeVec Rational LW] -> FreeVec Rational LW
+            concatLC = foldr1 agAdd
+        
+
+
+lwAdjoint :: LW -> LW -> FreeVec Rational LW
+lwAdjoint w z = 
+  if w == z 
+    then LC [(0,w)]
+    else if z<w
+      then (fNeg fMId::Rational) `vScal` (bracket (LC [(1,z)]) (LC [(1,w)]) )
       else if (deg w == 1)
-        then [(1,LW $ (theWord w) ++ (theWord z))]
+        then LC [(1, LW $ (theWord w) ++ (theWord z))]
         else
-          [(fAId,fAId)]
-  )
+          case (not (y<z)) of
+            True -> LC [(1, LW $ (theWord w) ++ (theWord z))]
+            False -> (bracket (LC [(fMId::Rational,x)]) (lwAdjoint y z)) `agAdd` 
+              (bracket (lwAdjoint x z) (LC [(fMId,y)]))
+          where (x,y) = lyndonFactorization w
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,62 @@
+import Test.Hspec
+import Test.QuickCheck
+import FreeLie
+
+instance Arbitrary LW where
+  arbitrary = do
+    n <- choose (1::Int,3)
+    m <- choose (2::Int, 3)
+    let words = allLyndonWords n [1,2..m]
+    index <- choose (0, (length words) - 1 )
+    return (words!!index)
+
+instance (Arbitrary a, Arbitrary b) => Arbitrary (FreeVec a b) where
+  arbitrary = do 
+    x <- arbitrary
+    y <- arbitrary
+    return (LC [(x,y)])
+
+
+--Vector spaces should form an abelian group under addition
+agAssocLaw :: (AbelianGroup a, Eq a) => a-> a-> a -> Bool
+agAssocLaw x y z = (x `agAdd` (y `agAdd` z)) == ((x `agAdd` y) `agAdd` z)
+
+--agUnitLaw :: (AbelianGroup a, Eq a) => a -> Bool
+--agUnitLaw x = and [(agId `agAdd` x) == x, (x `agAdd` agId) == x]
+
+agUnitLaw :: (FreeVec Rational LW) -> Bool
+agUnitLaw x = and [ simplify (agId `agAdd` x) == simplify x, simplify (x `agAdd` agId) == simplify x]
+
+agAbelianLaw :: (AbelianGroup a, Eq a) => a -> a -> Bool
+agAbelianLaw x y = (x `agAdd` y) == (y `agAdd` x)
+
+agInverseLaw :: (AbelianGroup a, Eq a) => a -> Bool
+agInverseLaw x = (x `agAdd` (agInv x)) == agId
+
+lieAlgebraJacobi :: (FreeVec Rational LW) -> (FreeVec Rational LW)
+  -> (FreeVec Rational LW) -> Bool
+lieAlgebraJacobi x y z =
+  bracket x (bracket y z) `agAdd` bracket y (bracket z x)
+    `agAdd` bracket z (bracket x y) == (agId::(FreeVec Rational LW))
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do 
+  describe "Verifying mathematical structures satify defining axioms:" $ do
+
+    it "FreeVec Rational LW is abelian" $ do
+      property $ (agAbelianLaw :: (FreeVec Rational LW) -> (FreeVec Rational LW) -> Bool)
+
+    it "FreeVec Rational LW safies additive associativity" $ do
+      property $ (agAssocLaw :: (FreeVec Rational LW) -> (FreeVec Rational LW) 
+        -> (FreeVec Rational LW) -> Bool) 
+
+    it "FreeVec Rational LW safies additive unit laws" $ do
+      property $ (agUnitLaw :: (FreeVec Rational LW) -> Bool) 
+    
+    it "FreeVec Rational LW safies additive inverse laws" $ do
+      property $ (agInverseLaw :: (FreeVec Rational LW) -> Bool) 
+
+    it "FreeVec Rational LW satisfies Jacobi Identity" $
+      property $ lieAlgebraJacobi
+    
+
